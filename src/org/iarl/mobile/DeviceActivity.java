00fe1b8cf6e936a8275dd638dfcef63e829b8290
zarya <zarya@berenklouw.client.home.gigafreak.net>
2012-01-30 13:00:00 +0100

    Added gps support

--- src/org/iarl/mobile/DeviceActivity.java
+++ src/org/iarl/mobile/DeviceActivity.java
@@ -1,6 +1,7 @@
 package org.iarl.mobile;
 
 import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -19,6 +20,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -60,6 +65,15 @@ public class DeviceActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        //Start GPS
+        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LocationListener mlocListener = new MyLocationListener();
+        //Create gps accuracy criteria
+        Criteria coarse = new Criteria();
+        coarse.setAccuracy(Criteria.ACCURACY_COARSE);
+        
+        mlocManager.requestLocationUpdates( mlocManager.getBestProvider(coarse, true), 500, 1000, mlocListener);
+        
         // Initialize cache
         cache = new DeviceCacheHelper(this);
 
@@ -94,7 +108,7 @@ public class DeviceActivity extends Activity {
         });
     }
 
-    private void getLocationByIp() {
+	private void getLocationByIp() {
         server.getJson("meta/client/info/android/" + Build.VERSION.RELEASE,
             new JsonHttpResponseHandler() {
                 @Override
@@ -208,5 +222,34 @@ public class DeviceActivity extends Activity {
             Collections.sort(devices, new DeviceComparator());
         }
         updateDevices();
+    }
+    
+    public class MyLocationListener implements LocationListener
+    {
+    	
+    	@Override
+    	public void onLocationChanged(Location loc)
+    	{
+            Coordinates newLocation = new Coordinates(loc.getLongitude(),loc.getLatitude());
+            updateLocation(newLocation);
+    	}
+
+    	@Override
+    	public void onProviderDisabled(String provider)
+    	{
+    		Log.e(null,"GPS Disabled");
+    	}
+
+    	@Override
+    	public void onProviderEnabled(String provider)
+    	{
+    		Log.d(null,"GPS Enabled");
+    	}
+
+    	@Override
+    	public void onStatusChanged(String provider, int status, Bundle extras)
+    	{
+    	}
+
     }
 }
